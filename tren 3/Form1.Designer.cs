e7ddb7f6ddb7e4c22095355876bddca796199ed2
jaros <jaros@TRIGGER>
2023-04-14 00:54:49 +0300

    Добавьте файлы проекта.

---
+++ tren 3/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace tren_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Photo = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Info = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Discount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Photo,
+            this.Info,
+            this.Price,
+            this.Discount});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 114);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowTemplate.Height = 60;
+            this.dataGridView1.Size = new System.Drawing.Size(723, 324);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Без фильтра",
+            "По возрастанию",
+            "По убыванию"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Без фильтра",
+            "Скидка больше 2%",
+            "Скидка больше 4%"});
+            this.comboBox2.Location = new System.Drawing.Point(158, 78);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(317, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // Photo
+            // 
+            this.Photo.HeaderText = "Фото";
+            this.Photo.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Photo.Name = "Photo";
+            // 
+            // Info
+            // 
+            this.Info.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Info.HeaderText = "Описание";
+            this.Info.Name = "Info";
+            this.Info.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Цена";
+            this.Price.Name = "Price";
+            // 
+            // Discount
+            // 
+            this.Discount.HeaderText = "Цена со скидкой";
+            this.Discount.Name = "Discount";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewImageColumn Photo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Info;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Discount;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ tren 3/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tren_3.Properties;
+
+namespace tren_3
+{
+    public partial class Form1 : Form
+    {
+        User02_04Entities entities = new User02_04Entities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        int allproductCount;
+        List<Product> pr;
+
+        public void Sort() 
+        {
+            pr = entities.Product.ToList();
+
+            switch (comboBox1.SelectedIndex) 
+            {
+                case 0:
+
+                    pr = pr.ToList();
+                    allproductCount = pr.Count;
+                    break;
+                case 1:
+                    pr = pr.OrderBy(x => Convert.ToInt32(x.Price)).ToList();
+                    break;
+                case 2:
+                    pr = pr.OrderByDescending(x => Convert.ToInt32(x.Price)).ToList();
+                    break;
+            }
+            switch (comboBox2.SelectedIndex) 
+            {
+                case 0:
+                    pr = pr.ToList();
+                    break;
+
+                case 1:
+                    pr = pr.Where(x => Convert.ToInt32(x.DiscountNow) > 2).ToList();
+                    break;
+
+                case 2:
+                    pr = pr.Where(x => Convert.ToInt32(x.DiscountNow) >= 4).ToList();
+                    break;
+            }
+
+            for (int i = 0; i < pr.Count; i++) 
+            {
+                dataGridView1.Rows.Add();
+
+
+                string photoname = pr.Select(x => x.ProductImage).ToArray()[i];
+                if(String.IsNullOrEmpty(photoname))
+                    dataGridView1.Rows[i].Cells[0].Value = Resources.pictur;
+                else
+                    dataGridView1.Rows[i].Cells[0].Value = Resources.ResourceManager.GetObject(photoname);
+
+                dataGridView1.Rows[i].Cells[1].Value = $"Назваение товара: {pr.Select(x => x.Name).ToArray()[i]}\n" +
+                    $"{pr.Select(x => x.Manufacture.ManufactureName).ToArray()[i]}";
+
+                dataGridView1.Rows[i].Cells[2].Value = pr.Select(x => x.Price).ToArray()[i];
+                dataGridView1.Rows[i].Cells[3].Value = pr.Select(x => x.DiscountNow).ToArray()[i];
+            }
+
+            label1.Text = dataGridView1.Rows.Count + " из " + allproductCount;
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            Sort();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            Sort();
+        }
+    }
+}
